dad3d0c7ba5e2825be5ae7857938108cae42a248
Randall Raboy <contact@randallraboy.com>
2026-01-17 13:28:27 -0500

    upgrade angular@19 and fontawesome

--- web/src/app/components/category/category.component.ts
+++ web/src/app/components/category/category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrl: './category.component.scss'
+    selector: 'app-category',
+    templateUrl: './category.component.html',
+    styleUrl: './category.component.scss',
+    standalone: false
 })
 export class CategoryComponent {
 
